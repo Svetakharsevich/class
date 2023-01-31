134f1f0146eaa43edc6ebf4fec1078c344d24500
Svetakharsevich <svetlanakharsevich@gmail.com>
2023-01-31 20:59:14 +0500

    first commit

---
+++ класс/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace класс
+{
+    public class Tel
+    {
+        //задание 1
+        public int day;
+        public double time;
+        public double money;
+        public double calculator()
+        {
+            double totalPrice = time * money;
+            if (day == 6 || day == 7)
+                return totalPrice - totalPrice * 0.1;
+            else return totalPrice;
+        }
+    }
+    //задание 3
+    public class Sqr
+    {
+        public void Generation()
+        {
+            int square;
+            for (int i = 1; i <= 10; i++)
+            {
+                square = Convert.ToInt32(Math.Pow(i, 2));
+                Console.Write(i + " ");
+                EndSqr ss = new EndSqr();
+                ss.Stop(square);
+            }
+        }
+    }
+
+    public class EndSqr
+    {
+        public void Stop(int number)
+        {
+            if (number >= 25)
+            {
+                Console.WriteLine("Квадрат числа больше 25!");
+                Console.ReadKey();
+            }
+
+        }
+    }
+
+}
---
+++ класс/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace класс
+{
+    internal class Program
+    {
+        static void TaskOne()
+        {
+            try
+            {
+                Tel tel = new Tel();
+                while (tel.day < 1 || tel.day > 7)
+                {
+                    Console.Write("День недели разговора: ");
+                    tel.day = Convert.ToInt32(Console.ReadLine());
+                    if (tel.day < 1 || tel.day > 7)
+                    {
+                        Console.WriteLine("День должен быть от 1 до 7");//задание 2
+                        Console.ReadKey();
+                    }
+
+                }
+                Console.Write("Время разговора: ");
+                tel.time = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Стоимость минуты:");
+                tel.money = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"Стоимость разговора: {tel.calculator()}");
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка");
+            }
+        }
+
+        static void TaskTree()
+        {
+            try
+            {
+                Sqr number = new Sqr();
+                number.Generation();
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка");
+            }
+        }
+        static void Main(string[] args)
+        {
+            TaskOne();
+            TaskTree();
+            Console.ReadKey();
+        }
+    }
+}
